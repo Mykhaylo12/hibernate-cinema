b753c3bbbcbfcc04915e8a10ffe46cc80b229a2c
Mykhaylo12 <55308826+Mykhaylo12@users.noreply.github.com>
2020-02-05 15:18:23 +0200

    hw27.1 (#2)
    * hw27.1
    
    * removed loggers from daoImpl

--- src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
@@ -1,8 +1,8 @@
 package com.dev.cinema.dao.impl;
 
-import com.dev.cinema.dao.MovieDao;
+import com.dev.cinema.dao.CinemaHallDao;
 import com.dev.cinema.lib.Dao;
-import com.dev.cinema.model.Movie;
+import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.util.HibernateUtil;
 import java.util.List;
 import javax.persistence.criteria.CriteriaQuery;
@@ -10,33 +10,33 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Dao
-public class MovieDaoImpl implements MovieDao {
+public class CinemaHallDaoImpl implements CinemaHallDao {
     @Override
-    public Movie add(Movie movie) {
+    public CinemaHall add(CinemaHall cinemaHall) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            Long itemId = (Long) session.save(movie);
+            Long itemId = (Long) session.save(cinemaHall);
             transaction.commit();
-            movie.setId(itemId);
-            return movie;
+            cinemaHall.setId(itemId);
+            return cinemaHall;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't add movie to DB ", e);
+            throw new RuntimeException("Can't add cinema hall to DB", e);
         }
     }
 
     @Override
-    public List<Movie> getAll() {
+    public List<CinemaHall> getAll() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            CriteriaQuery<Movie> criteriaQuery = session.getCriteriaBuilder()
-                    .createQuery(Movie.class);
-            criteriaQuery.from(Movie.class);
+            CriteriaQuery<CinemaHall> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(CinemaHall.class);
+            criteriaQuery.from(CinemaHall.class);
             return session.createQuery(criteriaQuery).getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Can't get all movies from DB ", e);
+            throw new RuntimeException("Can't get all cinema halls", e);
         }
     }
 }
