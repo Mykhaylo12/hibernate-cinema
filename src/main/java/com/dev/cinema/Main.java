ac77cbfada6b0eb7d512de28a1ea43b71082b521
Mykhaylo12 <55308826+Mykhaylo12@users.noreply.github.com>
2020-02-06 12:44:44 +0200

    hw27.2 (#3)
    * hw27.2
    
    * removed predicate from method findByLogin in UserDaoImpl.java

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -4,10 +4,13 @@ import com.dev.cinema.lib.Injector;
 import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.AuthenticationService;
 import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
 import com.dev.cinema.service.MovieSessionService;
 import java.time.LocalDateTime;
+import javax.naming.AuthenticationException;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.dev.cinema");
@@ -38,5 +41,17 @@ public class Main {
         System.out.println(cinemaHall);
         movieSessionService.findAvailableSessions(movie.getId(), showTime.toLocalDate())
                 .forEach(System.out::println);
+
+        AuthenticationService authenticationService = (AuthenticationService) injector
+                .getInstance(AuthenticationService.class);
+        User newRegistratedUser = authenticationService.register("email", "1");
+        System.out.println(newRegistratedUser);
+        User userAfterLogin = null;
+        try {
+            userAfterLogin = authenticationService.login("email", "1");
+        } catch (AuthenticationException e) {
+            System.out.println("Incorrect email or password");
+        }
+        System.out.println(userAfterLogin);
     }
 }
---
+++ src/main/java/com/dev/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,45 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.UserDao;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long userId = (Long) session.save(user);
+            transaction.commit();
+            user.setId(userId);
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create add user to DB");
+        }
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<User> cq = cb.createQuery(User.class);
+            Root<User> root = cq.from(User.class);
+            cq.select(root).where(cb.and(cb.equal(root.get("email"), email)));
+            return session.createQuery(cq).uniqueResult();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find user by email" + e);
+        }
+    }
+}
