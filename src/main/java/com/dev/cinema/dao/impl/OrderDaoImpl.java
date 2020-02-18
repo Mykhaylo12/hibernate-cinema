28d7f52dff6f2f57c9237c3352778507d3c69590
Mykhailo Kramar <3805033@gmail.com>
2020-02-18 12:34:25 +0200

    hw 32.2

--- src/main/java/com/dev/cinema/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/OrderDaoImpl.java
@@ -1,20 +1,24 @@
 package com.dev.cinema.dao.impl;
 
 import com.dev.cinema.dao.OrderDao;
-import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.Order;
 import com.dev.cinema.model.User;
-import com.dev.cinema.util.HibernateUtil;
 import java.util.List;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-@Dao
+@Repository
 public class OrderDaoImpl implements OrderDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @Override
     public Order add(Order order) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             Long orderId = (Long) session.save(order);
             transaction.commit();
@@ -30,7 +34,7 @@ public class OrderDaoImpl implements OrderDao {
 
     @Override
     public List<Order> getOrderHistory(User user) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             return session.createQuery("FROM orders WHERE user_id=:user_id", Order.class)
                     .setParameter("user_id", user.getId()).getResultList();
         } catch (Exception e) {
--- src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
@@ -2,8 +2,6 @@ package com.dev.cinema.service.impl;
 
 import com.dev.cinema.dao.OrderDao;
 import com.dev.cinema.dao.ShoppingCartDao;
-import com.dev.cinema.lib.Inject;
-import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.Order;
 import com.dev.cinema.model.ShoppingCart;
 import com.dev.cinema.model.Ticket;
@@ -12,14 +10,16 @@ import com.dev.cinema.service.OrderService;
 import com.dev.cinema.service.ShoppingCartService;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-    @Inject
+    @Autowired
     private OrderDao orderDao;
-    @Inject
+    @Autowired
     private ShoppingCartDao shoppingCartDao;
-    @Inject
+    @Autowired
     private ShoppingCartService shoppingCartService;
 
     @Override
